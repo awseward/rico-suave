6d7db355f04709b9280636a19d68b0a45f00c860
Andrew William Seward <awswrd@gmail.com>
2016-04-30 14:46:47 -0700

    Some basic routes

--- build.fsx
+++ build.fsx
@@ -1,13 +1,31 @@
 #r @"./packages/FAKE/tools/FakeLib.dll"
 #r @"./packages/Suave/lib/net40/Suave.dll"
 
-open Fake
-open Suave
 open System
 open System.IO
 
-Target "Start" (fun _ ->
-  startWebServer defaultConfig (Successful.OK "Hi!")
-)
+open Fake
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open System.Net
 
-RunTargetOrDefault "Start"
+let port = Sockets.Port.Parse <| getBuildParamOrDefault "port" "8083"
+let config =
+  { defaultConfig with
+      bindings = [ HttpBinding.mk Protocol.HTTP IPAddress.Loopback port ]
+  }
+
+let app =
+  choose
+    [ GET >=> choose
+        [
+          path "/" >=> OK "GET /"
+          path "/hello" >=> OK "GET /hello"
+          path "/goodbye" >=> OK "GET /goodbye" ]
+      POST >=> choose
+        [ path "/hello" >=> OK "POST /hello"
+          path "/goodbye" >=> OK "POST /goodbye" ] ]
+
+startWebServer config app
