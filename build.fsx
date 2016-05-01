9e96226f1ddcbb7bfa518f5894bfa78712fd3a2d
Andrew William Seward <awswrd@gmail.com>
2016-05-01 13:38:27 -0700

    Proof of Concept for (recv,send)ing json
    Very simple. Nice helpful article here:
    http://blog.tamizhvendan.in/blog/2015/06/11/building-rest-api-in-fsharp-using-suave/

--- build.fsx
+++ build.fsx
@@ -1,5 +1,6 @@
 #r @"./packages/FAKE/tools/FakeLib.dll"
 #r @"./packages/Suave/lib/net40/Suave.dll"
+#r @"./packages/Newtonsoft.Json/lib/net40/Newtonsoft.Json.dll"
 
 open System
 open System.IO
@@ -10,6 +11,8 @@ open Suave.Filters
 open Suave.Operators
 open Suave.Successful
 open System.Net
+open Newtonsoft.Json
+open Newtonsoft.Json.Serialization
 
 let port = Sockets.Port.Parse <| getBuildParamOrDefault "port" "8083"
 let config =
@@ -17,17 +20,55 @@ let config =
       bindings = [ HttpBinding.mk Protocol.HTTP IPAddress.Loopback port ]
   }
 
+type Something =
+  {
+    Id : int
+    Name : string
+  }
+
+let someInstance =
+  {
+    Id = 123
+    Name = "This is a thing"
+  }
+
+let toJson v =
+  let serializerSettings = new JsonSerializerSettings()
+  serializerSettings.ContractResolver <-
+    new CamelCasePropertyNamesContractResolver()
+
+  JsonConvert.SerializeObject(v, serializerSettings)
+  |> OK
+  >=> Writers.setMimeType "application/json; charset=utf-8"
+
+let fromJson<'a> json =
+  JsonConvert.DeserializeObject(json, typeof<'a>) :?> 'a
+
+let jsonToSomething (req: HttpRequest) =
+  let getString rawForm =
+    System.Text.Encoding.UTF8.GetString(rawForm)
+
+  let something =
+    req.rawForm
+    |> getString
+    |> fromJson<Something>
+
+  sprintf "id is %d and name is %s" something.Id something.Name
+  |> OK
+
 let app =
   choose
     [ GET >=> choose
         [
           path "/" >=> OK "GET /"
           path "/hello" >=> OK "GET /hello"
-          path "/goodbye" >=> OK "GET /goodbye" ]
+          path "/goodbye" >=> OK "GET /goodbye"
+          path "/json" >=> toJson someInstance ]
       POST >=> choose
         [
           path "/" >=> OK "POST /"
           path "/hello" >=> OK "POST /hello"
-          path "/goodbye" >=> OK "POST /goodbye" ] ]
+          path "/goodbye" >=> OK "POST /goodbye"
+          path "/json" >=> request jsonToSomething ] ]
 
 startWebServer config app
